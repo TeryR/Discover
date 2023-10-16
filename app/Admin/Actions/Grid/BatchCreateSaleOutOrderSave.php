2609c0b271e72cd5bf65d8d944a3f7f164bfc830
TeryR <Terry_R@126.com>
2023-10-16 15:19:03 +0800

    Changes version 2.x

--- app/Admin/Actions/Grid/BatchCreateSaleOutOrderSave.php
+++ app/Admin/Actions/Grid/BatchCreateSaleOutOrderSave.php
@@ -66,7 +66,7 @@ class BatchCreateSaleOutOrderSave extends BatchAction
                 'sku_id'      => $saleItemModel->sku_id,
                 'should_num'  => $saleItemModel->should_num,
                 'price'       => $saleItemModel->price,
-                'stock_name'=>$saleItemModel->actual_num
+                'stock_num'=>$saleItemModel->actual_num
 //                'percent'     => $saleItemModel->percent,
 //                'standard'    => $saleItemModel->standard,
             ];
